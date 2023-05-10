0b123a5bf5c6c4ef1a92cadb3783e2116f1be1ae
arsenemadeit <114684889+arsenemadeit@users.noreply.github.com>
2023-05-10 11:17:33 +0100

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -18,7 +18,7 @@
   $contact = new PHP_Email_Form;
   $contact->ajax = true;
   
-  /*$contact->to = $arsenembanou26@gmail.com;*/
+  $contact->to = 'arsenembanou26@gmail.com';
   $contact->from_name = $_POST['name'];
   $contact->from_email = $_POST['email'];
   $contact->subject = $_POST['subject'];
@@ -37,7 +37,14 @@
   $contact->add_message( $_POST['email'], 'Email');
   $contact->add_message( $_POST['message'], 'Message', 10);
 
-  // echo $contact->send();
+  echo $contact->send();
+
+$contact->invalid_to_email = 'Email to (receiving email address) is empty or invalid!';
+$contact->invalid_from_name' = 'From Name is empty!';
+$contact->invalid_from_email' = 'Email from: is empty or invalid!';
+$contact->invalid_subject' = 'Subject is too short or empty!';
+$contact->short' = 'is too short or empty!'; // If the length check number is set and the provided message text is under the set length in the add_message() method call
+$contact->ajax_error' = 'Sorry, the request should be an Ajax POST'; // If ajax property is set true and the post method is not an AJAX call
 
   if (isset($_POST['button'])) echo "clicked";{
         $entete  = 'MIME-Version: 1.0' . "\r\n";
